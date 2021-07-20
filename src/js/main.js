45a9ca1907ce885c71f8e2fbb1405224ff30ce76
vany pupkin <vany_pupkin@mail.ru>
2021-07-20 13:47:36 +0600

    make header

--- src/js/main.js
+++ src/js/main.js
@@ -6,55 +6,52 @@
 @@include('vendor/_modernizr-webp.js');
 
 //
-@@include('vendor/_lightgallery.min.js' );
+// @include('vendor/_lightgallery.min.js' );
 
 //
-@@include('vendor/_smooth-scroll.js' );
+// @include('vendor/_smooth-scroll.js' );
 
 //
-@@include('vendor/_swiper.min.js' );
+// @include('vendor/_swiper.min.js' );
 
 
 // MODULS ===========================================================
 
 // модальные окна
-@@include('modules/_modal.js');
+// @include('modules/_modal.js');
 
 // img-to-bg
 @@include('modules/_img-to-bg.js');
 
 // анимация блоков при скроле
-@@include('modules/_animation-block-scrolling.js');
+// @include('modules/_animation-block-scrolling.js');
 
 // динамический адаптив
 @@include('modules/_dynamic-adaptive.js');
 
 // табы
-@@include('modules/_tabs.js');
+// @include('modules/_tabs.js');
 
 // спойлеры
-@@include('modules/_spoilers.js');
+// @include('modules/_spoilers.js');
 
 // слайдеры
 
 //галерея
-@@include('modules/_gallery.js');
+// @include('modules/_gallery.js');
 
 //скролл
-@@include('modules/_scroll.js');
+// @include('modules/_scroll.js');
 
 // работа с элементами формы
-@@include('modules/_forms.js');
-
-
-
+// @include('modules/_forms.js');
 
 
 //открытие закрытие выпадающего меню
 //@include('modules/_main-nav.js');
 
 //открытие закрытие выпадающего меню
-// @include('modules/_hamburger.js');
+@@include('modules/_hamburger.js');
 
 
 
--- src/js/modules/_hamburger.js
+++ src/js/modules/_hamburger.js
@@ -1,27 +1,26 @@
 
-var hamburger = document.querySelector('.page-header__burger');
-var mainNav = document.querySelector('.main-nav');
-var pageHeader = document.querySelector('.page-header');
+var hamburger = document.querySelector('.header__burger');
+var mobileMenu = document.querySelector('.header__mobile-menu');
+var pageHeader = document.querySelector('.header');
 var pageBody = document.querySelector('.page__body');
 
-hamburger.addEventListener('click', function() {
-  if (hamburger) {
+
+if (hamburger) {
+  hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('active');
-  }
+    if (mobileMenu) {
+      mobileMenu.classList.toggle('header__mobile-menu--open');
+    }
 
-  if (mainNav) {
-    mainNav.classList.toggle('main-nav--open');
-  }
+    // if (pageHeader) {
+    //   pageHeader.classList.toggle('page-header--active');
+    // }
 
-  if (pageHeader) {
-    pageHeader.classList.toggle('page-header--active');
-  }
-
-  if (pageBody) {
-    pageBody.classList.toggle('page__body--lock');
-  }
-});
-
+    if (pageBody) {
+      pageBody.classList.toggle('page__body--lock');
+    }
+  });
+}
 
 // var mainNavLink = document.querySelectorAll('.page-header a');
 // mainNavLink.forEach(function(item, i, arr) {
